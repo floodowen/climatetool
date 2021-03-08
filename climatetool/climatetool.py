038a43a011e79c9d4faff933b4272064017cc218
floodowen <77589707+floodowen@users.noreply.github.com>
2021-03-08 13:07:53 -0500

    Add files via upload

--- climatetool/climatetool.py
+++ climatetool/climatetool.py
@@ -3,4 +3,16 @@
 """
 Climate visualization tool
 """
+import pandas as pd
+import plotly.graph_objects as go
 
+df = pd.read_csv('https://github.com/floodowen/climatetool/blob/main/seaice.csv')
+
+fig = go.Figure(go.Scatter(x = df['Year'], y = df['Extent'],
+                  name='Sea Ice over time'))
+
+fig.update_layout(title='Sea Ice over Time',
+                   plot_bgcolor='rgb(230, 230,230)',
+                   showlegend=True)
+
+fig.show()
