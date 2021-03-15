22b55e5755e42c3efd5939337bff50ebb6324d46
athe1sm <jj3146@columbia.edu>
2021-03-15 21:39:24 +0800

    added a webapp module

---
+++ paired-programming.md
@@ -0,0 +1,10 @@
+# Paired programming
+### Goal of the project:
+    If I am right the goal of this project is to create a plot or graph to visualize the data to show climate changes by using `streamlit`, `pandas`, `numpy`, `matplotlib`, `meteostat`, `geopandas`, `folium`, and `scikit-learn` packages.
+
+### The data:
+    I believe the data will be a sebset of public-available geopraphic and climate data. I also think it is better to work with part of those data to make the whole process less time consuming.
+
+### Code:
+    I noticed that you are on the very beginning of this project so keep it up. I also found out that you are using `https://github.com/floodowen/climatetool/blob/main/seaice.csv` for the URL of your data file, and I suppose it is better to use `https://raw.githubusercontent.com/athe1sm/climatetool/main/seaice.csv` instead to avoid those headers(?).
+    I think a REST API might be helpful to your project.
---
+++ webapp/webapp.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+import pandas as pd
+import requests
+import json
+'''
+This is a startup on the construction of webapp. At this point you can use this to access two cropped datasets, and when the devemopment 
+goes on it can support the display of those plots 
+'''
+# create the app as an instance of the fastAPI class
+app = FastAPI()
+
+# load the database once when the server starts
+
+SEAICEDATA = pd.read_csv("/home/athe1sm/hacks/climatetool/seaice_cropped.csv") # Feel free to change this to an URL. I can't do this since I don't have access to github on wsl2
+GREENHOUSEDATA = pd.read_csv("/home/athe1sm/hacks/climatetool/greenhouse_gas_inventory_data_data_cropped.csv")
+# create a root endpoint that say's hello
+@app.get("/")
+def root():
+    "returns a hello world message"
+    return {"message": "If you can see this, the server is running"}
+
+# create another endpoint for returning iris data
+@app.get("/climatedata")
+def iris(category=seaice):
+    """
+    returns climate data in JSON with option to subselect a category
+    """
+    # get subset or full data
+    if category == "seaice":
+        data = SEAICEDATA
+    else:
+        data = GREENHOUSEDATA
+
+    # convert to JSON and return to endpoint
+    sdata = data.to_json(orient="index")
+    jdata = json.loads(sdata)
+    return jdata
